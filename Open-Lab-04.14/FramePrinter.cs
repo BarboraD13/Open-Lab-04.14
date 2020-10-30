bca9b6853b9bd5ccb96826ea77ea7603759c0cf5
BarboraD13 <71934521+BarboraD13@users.noreply.github.com>
2020-10-30 11:17:26 +0100

    ++

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +6,38 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            string frame = "";
+            int a = 0;
+
+            foreach (string b in strings)
+            {
+                if (a < b.Length)
+                    a = b.Length;
+            }
+
+            for (int i = 0; i < a + 4; i++)
+            {
+                frame += "*";
+            }
+
+            Console.WriteLine(frame);
+
+            foreach (string b in strings)
+            {
+                if (b.Length < a)
+                {
+                    string str = "";
+                    for (int i = 0; i < a - b.Length; i++)
+                    {
+                        str += " ";
+                    }
+                    Console.WriteLine("* " + b + str + " *");
+                }
+                else
+                    Console.WriteLine("* " + b + " *");
+            }
+
+            Console.WriteLine(frame);
         }
     }
 }
